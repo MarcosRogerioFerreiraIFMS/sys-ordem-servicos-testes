c4217b702069a0c5842f3e6839ca0fb642dae2e0
Marcos Rogerio Ferreira <2413666@ifms.edu.br>
2022-10-21 21:53:52 -0400

    findAll e findById

--- src/test/java/br/edu/ifms/ordem/services/TecnicoServiceTests.java
+++ src/test/java/br/edu/ifms/ordem/services/TecnicoServiceTests.java
@@ -1,19 +1,30 @@
 package br.edu.ifms.ordem.services;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import br.edu.ifms.ordem.dto.TecnicoDTO;
+import br.edu.ifms.ordem.entities.Tecnico;
 import br.edu.ifms.ordem.repositories.TecnicoRepository;
 import br.edu.ifms.ordem.services.exceptions.DataBaseException;
 import br.edu.ifms.ordem.services.exceptions.ResourceNotFoundException;
+import br.edu.ifms.ordem.tests.Factory;
 
 @ExtendWith(SpringExtension.class)
 public class TecnicoServiceTests {
@@ -28,17 +39,57 @@ public class TecnicoServiceTests {
 	private Long idInexistente;
 	private Long idDependente;
 	
+	private PageImpl<Tecnico> page;
+	private Tecnico tecnico;
+	
 	@BeforeEach
 	void setUp() throws Exception {
 		idExistente = 1L;
 		idInexistente = 100L;
 		idDependente = 10L;
 		
+		tecnico = Factory.novoTecnico();
+		page = new PageImpl<>(List.of(tecnico));
+		
 		//Configurar comportamento
+		// Metodos com Retorno
+		Mockito.when(repository.findAll((Pageable)ArgumentMatchers.any())).thenReturn(page);
+		
+		Mockito.when(repository.findById(idExistente))
+		                       .thenReturn(Optional.of(tecnico));
+		Mockito.when(repository.findById(idInexistente))
+        					   .thenReturn(Optional.empty());
+		
+		
+		// Metodos VOID
 		Mockito.doNothing().when(repository).deleteById(idExistente);
 		Mockito.doThrow(EmptyResultDataAccessException.class).when(repository).deleteById(idInexistente);
 		Mockito.doThrow(DataIntegrityViolationException.class).when(repository).deleteById(idDependente);
 		
+	}
+	
+	@Test
+	public void findAllPagedDeveriaRetornarUmaPagina() {
+		Pageable pageable = PageRequest.of(0, 10);
+		Page<TecnicoDTO> resultado = service.findAllPaged(pageable);
+		Assertions.assertNotNull(resultado);
+		Mockito.verify(repository, Mockito.times(1)).findAll(pageable);
+	}
+	
+	@Test
+	public void findByIdDeveriaRetornarUmTecnicoQuandoIdExistir() {
+		TecnicoDTO tecnico = service.findById(idExistente);
+		Assertions.assertNotNull(tecnico);
+		Mockito.verify(repository, Mockito.times(1)).findById(idExistente);
+	}
+	
+	@Test
+	public void findByIdDeveriaRetornarUmaExcecaoQuandoIdExistir() {
+		Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+			service.findById(idInexistente);
+		});
+		
+		Mockito.verify(repository, Mockito.times(1)).findById(idInexistente);
 	}
 	
 	@Test
@@ -74,6 +125,7 @@ public class TecnicoServiceTests {
 		
 	}
 
+	
 }
 
 

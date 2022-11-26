0cd5335d0f7968129990184fb4d4e5f3474f36cf
Marcos Rogerio Ferreira <2413666@ifms.edu.br>
2022-11-25 21:19:18 -0400

    Camada Web - insert e delete

--- src/test/java/br/edu/ifms/ordem/resources/TecnicoResourceTests.java
+++ src/test/java/br/edu/ifms/ordem/resources/TecnicoResourceTests.java
@@ -2,8 +2,12 @@ package br.edu.ifms.ordem.resources;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -24,6 +28,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import br.edu.ifms.ordem.dto.TecnicoDTO;
 import br.edu.ifms.ordem.services.TecnicoService;
+import br.edu.ifms.ordem.services.exceptions.DataBaseException;
 import br.edu.ifms.ordem.services.exceptions.ResourceNotFoundException;
 import br.edu.ifms.ordem.tests.Factory;
 
@@ -43,15 +48,18 @@ public class TecnicoResourceTests {
 	private TecnicoDTO tecnicoDTO;
 	private Long idExistente;
 	private Long idInexistente;
+	private Long idDependente;
 	
 	@BeforeEach
 	void setUp() throws Exception {
 		idExistente = 1L;
 		idInexistente = 100L;
+		idDependente = 10L;
 		
 		tecnicoDTO = Factory.novoTecnicoDTO();
 		page = new PageImpl<>(List.of(tecnicoDTO));
 		
+		//findAll
 		when(service.findAllPaged(any())).thenReturn(page);
 		
 		//findById(id)
@@ -59,8 +67,47 @@ public class TecnicoResourceTests {
 		when(service.findById(idInexistente)).thenThrow(ResourceNotFoundException.class);
 		
 		//Update
-		when(service.update(eq(idExistente), any())).thenReturn(tecnicoDTO);
+		//when(service.update(eq(idExistente), any())).thenReturn(tecnicoDTO);
 		when(service.update(eq(idInexistente), any())).thenThrow(ResourceNotFoundException.class);
+		
+		//Insert
+		when(service.insert(any())).thenReturn(tecnicoDTO);
+		
+		//Delete
+		doNothing().when(service).delete(idExistente);
+		doThrow(ResourceNotFoundException.class).when(service).delete(idInexistente);
+		doThrow(DataBaseException.class).when(service).delete(idDependente);
+		
+	}
+	
+	@Test
+	public void insertDeveriaRetornarUmCreatedEUmTecnicoDTO() throws Exception{
+		String jsonBody = objectMapper.writeValueAsString(tecnicoDTO);
+		
+		ResultActions resultado = mockMvc.perform(post("/tecnicos")
+					.content(jsonBody)
+					.contentType(MediaType.APPLICATION_JSON)
+					.accept(MediaType.APPLICATION_JSON));
+			
+			resultado.andExpect(status().isCreated());
+			resultado.andExpect(jsonPath("$.id").exists());
+	}
+	
+	@Test
+	public void deleteDeveriaRetornarUmNoContentQuandoIdExistente() throws Exception{		
+		
+		ResultActions resultado = mockMvc.perform(delete("/tecnicos/{id}", idExistente)					
+					.accept(MediaType.APPLICATION_JSON));
+			
+			resultado.andExpect(status().isNoContent());			
+	}
+	
+	@Test
+	public void deleteDeveriaRetornarUmNotFoundQuandoIdInexistente() throws Exception{
+		ResultActions resultado = mockMvc.perform(delete("/tecnicos/{id}", idInexistente)					
+				.accept(MediaType.APPLICATION_JSON));
+		
+		resultado.andExpect(status().isNotFound());
 	}
 	
 	@Test
@@ -88,7 +135,6 @@ public class TecnicoResourceTests {
 			
 			resultado.andExpect(status().isNotFound());
 	}
-	
 	
 	@Test
 	public void findAllDeveriaRetornarPaginaTecnicoDTO() throws Exception {

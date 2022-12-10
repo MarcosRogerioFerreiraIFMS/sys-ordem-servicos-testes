02e1948e2fd669692ac9bdf8bc529c3c59cec87b
Marcos Rogerio Ferreira <2413666@ifms.edu.br>
2022-12-09 20:53:15 -0400

    Testes de Integração - findAllPaged

--- src/test/java/br/edu/ifms/ordem/services/TecnicoServiceIntegracaoTests.java
+++ src/test/java/br/edu/ifms/ordem/services/TecnicoServiceIntegracaoTests.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.edu.ifms.ordem.dto.TecnicoDTO;
 import br.edu.ifms.ordem.repositories.TecnicoRepository;
@@ -59,14 +61,33 @@ public class TecnicoServiceIntegracaoTests {
 		Assertions.assertEquals(0, resultado.getNumber());
 		Assertions.assertEquals(10, resultado.getSize());
 		Assertions.assertEquals(totalTecnicos, resultado.getTotalElements());
-		
 	}
 	
 	//PERGUNTA
 	// Se na linha 38 foi deletado um registro no banco de dados
 	// quando foi consultado na linha 56 e testado na linha 61
 	// a quantidade era a mesma uma vez que a variável totalTecnicos
-	// não foi alterada
+	// não foi alterada - ROLLBACK
+	
+	@Test
+	public void findAllPagedDeveriaRetornarPaginaVaziaQuandoPageNaoExistir(){
+		
+		PageRequest pageRequest =  PageRequest.of(50, 10);
+		Page<TecnicoDTO> resultado = service.findAllPaged(pageRequest);
+		
+		Assertions.assertTrue(resultado.isEmpty());
+	}
+	
+	@Test
+	public void findAllPagedDeveriaRetornarPaginaOrdenadaPorNome(){
+		
+		PageRequest pageRequest =  PageRequest.of(0, 10, Sort.by("nome"));
+		Page<TecnicoDTO> resultado = service.findAllPaged(pageRequest);
+		
+		Assertions.assertFalse(resultado.isEmpty());
+		Assertions.assertEquals("Bruno Mazeo", resultado.getContent().get(0).getNome());
+		Assertions.assertEquals("Claudia Raia", resultado.getContent().get(1).getNome());
+	}
 	
 }
 
